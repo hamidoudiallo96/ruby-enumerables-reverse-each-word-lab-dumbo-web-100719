87870ba7f6ea2893ec45b425e561477148e9df75
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-27 18:16:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,14 @@ def reverse_each_word(sentence)
   new_result = result_array.join(" ")
   return new_result
 end
+
+
+def reverse_each_word(sentence)
+  result_array = []
+  new_sent = sentence.split(" ")
+  new_sent.collect do |word|
+    result_array << word.reverse
+  end
+  new_result = result_array.join(" ")
+  return new_result
+end
